0df6e9e29a32250959a76d3e7d623a1f6d066aa5
amir hossein rezayy <ah.rezayy@chmail.ir>
2022-10-07 10:20:56 +0330

    feat: add loader

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,38 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { createBrowserRouter, useLocation } from 'react-router-dom';
 
 // guards
 import GuestGuard from '../guards/GuestGuard.jsx';
 
+// components
+import LoadingScreen from '../components/LoadingScreen.jsx';
+import AuthGuard from '../guards/AuthGuard.jsx';
+
+// eslint-disable-next-line react/display-name
+const Loadable = (Component) => (props) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { pathname } = useLocation();
+
+  return (
+    <Suspense fallback={<LoadingScreen isDashboard={pathname.includes('/dashboard')} />}>
+      <Component {...props} />
+    </Suspense>
+  );
+};
+
 // routes
-import App from '../App.jsx';
-import Page404 from '../pages/Page404.jsx';
-import Login from '../pages/auth/Login.jsx';
+const Login = Loadable(lazy(() => import('../pages/auth/Login.jsx')));
+const Page404 = Loadable(lazy(() => import('../pages/Page404.jsx')));
+const App = Loadable(lazy(() => import('../App.jsx')));
 
 export default createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <AuthGuard>
+        <App />
+      </AuthGuard>
+    ),
   },
   {
     path: '/login',
